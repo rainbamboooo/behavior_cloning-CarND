6e50edcf5b55bc86e09780aeaa67ed57d96839d4
Yuzhu Li <rainbamboo554155@hotmail.com>
2019-06-25 22:20:17 -0700

    Update model.py
    delete a stupid comment

--- model.py
+++ model.py
@@ -80,7 +80,7 @@ from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
-#煞笔李雨竹
+
 model = Sequential()
 model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
